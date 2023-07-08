ccdb344d30d583b9a5835cf8f3c048ebb598f22f
kwerdna19 <kuczynskiandrew@yahoo.com>
2023-07-07 20:13:58 -0400

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
-import { resolve, basename } from 'path'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import * as packageJson from './package.json'
-import * as tsConfig from './tsconfig.json'
-
 
 export default defineConfig({
   plugins: [react(), dts({
     insertTypesEntry: true,
     copyDtsFiles: true,
-    beforeWriteFile(filePath, content) {
-      const output = basename(packageJson.types)
-      if(basename(filePath) === output) {
-        const typePathImports = tsConfig.compilerOptions.types.map(t => `import '${t}'`)
-        return {
-          filePath,
-          content: `${typePathImports.join('\n')}\n\n${content}`
-        }
-      }
-        
-    },
-
   })],
   build: {
     lib: {
